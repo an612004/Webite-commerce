25765593852a6a3a356526f7e50e46de768b0387
an612004 <hobinhan321@gmail.com>
2025-08-18 21:59:25 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,36 @@
+
+console.log("Hello, world!");
+// in ra thông báo "Hello, world!" trong màn hình console
+
+confirm("Hi javascript!");
+// hiển thị hộp thoại xác nhận với thông điệp "Hi javascript!"
+
+prompt("Nhập tên của bạn");
+// hiển thị hộp thoại nhập liệu với thông điệp "Nhập tên của bạn"
+
+
+setTimeout(function() {
+alert("Hello after 1 second");
+// hiện thị thông báo lên màn hình
+}, 1000);
+// => hiển thị hộp thoại thông báo sau 1 giây
+
+
+var a = 1 * 2;
+console.log(a);
+// in ra giá trị của biến a trong màn hình console
+
+// TOÁN TỬ GÁN
+var a = 2;
+var b = 3;
+
+if (a < b) {
+    alert("a nhỏ hơn b");
+}
+// => hiển thị hộp thoại thông báo nếu a nhỏ hơn b
+
+// TOÁN TỬ SO SÁNH
+
+var a = 2;
+
+console.alert(a);
